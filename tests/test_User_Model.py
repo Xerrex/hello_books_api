ebf8a8135137e5b260f41245a237c8b4ed7a5b37
Alex Kagai <alexkagai@outlook.com>
2018-03-30 14:39:08 +0300

    [feature #156039485] Add tests for user register

--- tests/test_User_Model.py
+++ tests/test_User_Model.py
@@ -3,16 +3,16 @@ Test Operations Involving the user model
 Login , Register too
 """
 from unittest import main
+import json
 
 from tests import TestBase
 from app.models.user import User
+from app.views import USERS
 
 
 class UserModelCase(TestBase):
 
-    def setUp(self):
-        self.user1 = User('Alex','alex@dev.com','12345678','dev with mad skills')
-
+    user1 = User('Alex','alex@dev.com','12345678','dev with mad skills')
 
     def test_User_init_is_accurate(self):
         """
@@ -32,7 +32,53 @@ class UserModelCase(TestBase):
         self.assertFalse(user_password_hashed == user_password_string)
         self.assertTrue(self.user1.verify_password(user_password_string))
 
-        
+    def test_user_registration(self):
+
+        """Test user registration
+
+        Assert that a valid POST request to /api/v1/auth/register
+        registers a new user
+        """
+
+        new_user = {
+            "name": "Alex2",
+            "email": "alex@dev.com2",
+            "password": "1234567892",
+            "aboutme": "mad skills you2"
+        }
+
+        response = self.client.post('/api/v1/auth/register', data=json.dumps(new_user),
+                                     content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("User registration was successful", response.get_data(as_text=True))
+
+
+
+    def test_no_duplicate_user_registration(self):
+
+        """Test no duplicate user registration
+
+        Assert that a valid POST request to /api/v1/auth/register
+        fails with error 409 and message in context with the same.
+        """
+
+        new_user = {
+            "name": "Alex",
+            "email": "alex@dev.com",
+            "password": "123456789",
+            "aboutme": "mad skills you"
+        }
+        response = self.client.post('/api/v1/auth/register', data=json.dumps(new_user),
+                                    content_type='application/json')
+
+        response = self.client.post('/api/v1/auth/register', data=json.dumps(new_user),
+                                    content_type='application/json')
+
+        self.assertTrue(response.status_code == 409)
+        self.assertIn('User with that email already exists',response.get_data(as_text=True))
+
+
 
 if __name__ == '__main__':
     main(verbosity=2)

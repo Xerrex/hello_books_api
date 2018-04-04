ad3aa991d78a18cd1f0aafeb69d5f07898086e0f
Alex Kagai <alexkagai@outlook.com>
2018-04-04 06:11:26 +0300

    [chore #156406804] Break down Views into blueprints

---
+++ app/auth/__init__.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+from flask_restful import Api
+
+from .views import RegisterResource, LoginResource, \
+    LogoutResource, ResetPasswordResource
+
+auth_Bp = Blueprint('auth', __name__, url_prefix='/api/v1/auth')
+
+api = Api(auth_Bp)
+
+api.add_resource(RegisterResource, '/register', endpoint="register")
+
+api.add_resource(LoginResource, '/login', endpoint="login")
+
+api.add_resource(LogoutResource, '/logout', endpoint='logout')
+
+api.add_resource(ResetPasswordResource, '/reset-password',
+                 endpoint='reset-password')
